9820ee93c632124ba889b4681a59b7db1849c246
SreytouchLang <atouch.it@gmail.com>
2023-12-11 17:24:20 -0800

    Initial Commit

---
+++ SwiftUI-Login-MVVM/SwiftUI_Login_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Login_MVVMApp.swift
+//  SwiftUI-Login-MVVM
+//
+//  Created by Sreytouch(Jessica) on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Login_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
